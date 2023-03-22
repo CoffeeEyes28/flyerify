6b6836ee052324ed78058b2ee99dbb4d89d16b6f
Greg <litm47@gmail.com>
2023-03-22 15:34:38 -0400

    expire check now works

--- flyerify/src/App.js
+++ flyerify/src/App.js
@@ -93,7 +93,7 @@ function App() {
   const checkExpire = () => {
     
     let currentTime = new Date()
-    let expireTime = localStorage.getItem("expire")
+    let expireTime = new Date(localStorage.getItem("expire"))
     
 
     if (expireTime < currentTime ){
